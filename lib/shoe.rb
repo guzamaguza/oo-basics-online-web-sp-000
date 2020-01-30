77a6d4c49bd6c3a6da5c56d5cf0b1d304914bba0
Mark A Guza <guzamaguza@gmail.com>
2020-01-30 03:53:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color
+    @color
+  end
+
+  def size
+    @size
+  end
+
+  def material
+    @material
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble
+    puts "Shoe has been repaired."
+    @condition = new
+  end
+end
